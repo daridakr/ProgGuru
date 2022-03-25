b1aca50f35341c3a8111cf56c95be55fdaee3408
даша Даша <DaridaKr@gmail.com>
2022-03-25 18:06:45 +0300

    CommentOutDebugStatements

--- src/Daridakr.ProgGuru.Web/Program.cs
+++ src/Daridakr.ProgGuru.Web/Program.cs
@@ -13,18 +13,18 @@ public class Program
     public async static Task<int> Main(string[] args)
     {
         Log.Logger = new LoggerConfiguration()
-#if DEBUG
+//#if DEBUG
             .MinimumLevel.Debug()
-#else
+//#else
             .MinimumLevel.Information()
-#endif
+//#endif
             .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
             .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
             .Enrich.FromLogContext()
             .WriteTo.Async(c => c.File("Logs/logs.txt"))
-#if DEBUG
+//#if DEBUG
             .WriteTo.Async(c => c.Console())
-#endif
+//#endif
             .CreateLogger();
 
         try
